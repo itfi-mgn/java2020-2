3d6340fd89d8993c6a5039322bcef4fe1fe9450e
Alexander Chernomyrdin <chav1961@mail.ru>
2020-11-24 19:06:08 +0500

    Daily commit

---
+++ src/lesson9/Exercises.java
@@ -0,0 +1,16 @@
+package lesson9;
+
+public class Exercises {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1. Make file d:/test.txt with content:
+		//    ivanov,20000
+		//    petroff,25000
+		//    sidoroff,10000
+		// 2. Read content and calculate total salary
+		// String.split(...)
+		// Integer.valueOf(...)
+	}
+
+}
