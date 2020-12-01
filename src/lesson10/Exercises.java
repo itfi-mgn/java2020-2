ba1e220e88097e38a3a2a1b0bd6620529951044b
Alexander Chernomyrdin <chav1961@mail.ru>
2020-12-01 18:54:47 +0500

    Daily commit

---
+++ src/lesson10/Exercises.java
@@ -0,0 +1,17 @@
+package lesson10;
+
+public class Exercises {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1. Create zip file d:/test.zip, and two parts in it:
+		//	- part1
+		//    ivanov,20000
+		//    petroff,25000
+		//    sidoroff,10000
+		//	- part2
+		//    vasechkin,10000
+		//    kim,30000
+		//    tsoy,40000
+	}
+}
