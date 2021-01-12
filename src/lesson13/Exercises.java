21fae7bacd5d6307a2d0a7fd783c91fd32a58637
Alexander Chernomyrdin <chav1961@mail.ru>
2021-01-12 18:57:01 +0500

    Daily commit

---
+++ src/lesson13/Exercises.java
@@ -0,0 +1,12 @@
+package lesson13;
+
+public class Exercises {
+	public static String[]	names = {"ivan","petr","boris","petr","alla","ivan"};
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// exclude duplicates from 'names' and sort it
+	}
+
+}
